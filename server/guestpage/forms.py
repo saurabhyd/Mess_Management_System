bf87ad706eaa39b84f68885a5f601389b8939e78
Kiran S <krnsnaik@gmail.com>
2017-10-30 01:44:12 +0530

    Guest Registration updated

---
+++ server/guestpage/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import guestreg
+MESS_CHOICES = [
+    (0,'---Select Mess Block---'),
+    (1, 'Mess Block 1'),
+    (2, 'Mess Block 2'),
+    (3, 'Mess Block 3'),
+    (4, 'Mess Block 4'),
+    (5, 'Mess Block 5'),
+]
+
+TYPE = [
+    ('zero','---Select Meal Type---'),
+    ('Breakfast','Breakfast'),
+    ('Lunch','Lunch'),
+    ('Dinner','Dinner'),
+]
+
+
+
+class guestform(forms.ModelForm):
+    fname = forms.CharField(label='',widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
+    lname = forms.CharField(label='',widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
+    block = forms.CharField(label='',widget=forms.Select(choices=MESS_CHOICES,attrs={'class':'messblock'}))
+    meal = forms.CharField(label='',widget=forms.Select(choices=TYPE,attrs={'class':'mealtype'}))
+    date = forms.DateField()
+
+    class Meta:
+        model=guestreg
+        fields = ('fname','lname','block','meal','date')
--- server/guestpage/views.py
+++ server/guestpage/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .forms import guestform
+from .models import guestreg
 
 # Create your views here.
 def guest(request):
-    return render(request, 'guestpage/guest.html')
+
+    if request.method == 'POST':
+        form = guestform(request.POST)
+        if form.is_valid():
+            #if form.block!=0 and form.meal!='zero':
+            form.save()
+            return HttpResponse('<div class="alert alert-success"><strong>Success!</strong> Your Meal has been Booked</div>')
+            #else:
+                #return render(request, 'guestpage/guest.html', {'form': form})
+    form=guestform()
+    return render(request, 'guestpage/guest.html', {'form':form})
